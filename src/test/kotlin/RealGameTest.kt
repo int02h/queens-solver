4ed617cadd78679041425e1def4518dae11567bd
Daniil Popov <int02h@gmail.com>
2025-08-01 17:40:19 +0300

    new real tests

--- src/test/kotlin/RealGameTest.kt
+++ src/test/kotlin/RealGameTest.kt
@@ -374,6 +374,64 @@ class RealGameTest {
         )
     }
 
+    @Test
+    fun `2025-Jul-31`() {
+        val queens = solve(
+            "pobbggggg",
+            "ooobbgggg",
+            "rorwwwggg",
+            "rrrrwgggg",
+            "rryyyyygg",
+            "ryyyyyddg",
+            "ryyyyyydv",
+            "rryyyyyvv",
+            "rrrrrrvvv",
+        )
+        assertEquals(
+            setOf(
+                Position(row = 0, col = 0),
+                Position(row = 1, col = 3),
+                Position(row = 2, col = 1),
+                Position(row = 3, col = 4),
+                Position(row = 4, col = 8),
+                Position(row = 5, col = 6),
+                Position(row = 6, col = 2),
+                Position(row = 7, col = 7),
+                Position(row = 8, col = 5),
+            ),
+            queens
+        )
+    }
+
+    @Test
+    fun `2025-Aug-01`() {
+        val queens = solve(
+            "bbbpppppp",
+            "bbbpppppp",
+            "bvbpppppp",
+            "bbbrrrppp",
+            "bbbrwrggg",
+            "wwwrwrgyy",
+            "wdwrwrggg",
+            "ddwrwrgoo",
+            "ddwwwwggg",
+        )
+        assertEquals(
+            setOf(
+                Position(row = 0, col = 2),
+                Position(row = 1, col = 5),
+                Position(row = 2, col = 1),
+                Position(row = 3, col = 3),
+                Position(row = 4, col = 6),
+                Position(row = 5, col = 8),
+                Position(row = 6, col = 4),
+                Position(row = 7, col = 7),
+                Position(row = 8, col = 0),
+            ),
+            queens
+        )
+    }
+
     private fun solve(vararg encodedField: String): Set<Position> {
         val field = decodeField(encodedField.toList())
         val allSolutions = SolutionFinder().findAllSolutions(field)
