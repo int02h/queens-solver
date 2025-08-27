1222c898f83573b94b3e4a0ba7da815ec72c4d66
Daniil Popov <int02h@gmail.com>
2025-08-27 12:56:53 +0300

    new test

--- src/test/kotlin/RealGameTest.kt
+++ src/test/kotlin/RealGameTest.kt
@@ -849,6 +849,35 @@ class RealGameTest {
         )
     }
 
+    @Test
+    fun `2025-Aug-27`() {
+        val queens = solve(
+            "ppppppppp",
+            "ppppvpppp",
+            "ppbbggppp",
+            "pbbwwggpp",
+            "bbwwryggp",
+            "bwwwryggg",
+            "dwwwryyyy",
+            "dddrrrooy",
+            "drrrrrroo"
+        )
+        assertEquals(
+            setOf(
+                Position(row = 0, col = 8),
+                Position(row = 1, col = 4),
+                Position(row = 2, col = 2),
+                Position(row = 3, col = 6),
+                Position(row = 4, col = 3),
+                Position(row = 5, col = 5),
+                Position(row = 6, col = 0),
+                Position(row = 7, col = 7),
+                Position(row = 8, col = 1),
+            ),
+            queens
+        )
+    }
+
     private fun solve(vararg encodedField: String): Set<Position> {
         val field = FieldCodec.decodeFromHumanText(encodedField.toList())
         val allSolutions = SolutionFinder().findAllSolutions(field)
